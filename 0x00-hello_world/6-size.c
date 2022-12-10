44fb99a2cbeff6f1dee5f2a57edb268f3a90b0de
ProgressAce <daiyaansallie7@gmail.com>
2022-12-10 11:30:19 -0800

    Add 6-size.c program

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char;
+	int;
+	long int;
+	long long int;
+	float;
+	printf("Size of a char: %d byte(s)",sizeof(char));
+	printf("Size of a char: %d byte(s)",sizeof(char));
+	printf("Size of a char: %d byte(s)",sizeof(char));
+	printf("Size of a char: %d byte(s)",sizeof(char));
+	printf("Size of a char: %d byte(s)",sizeof(char));
+	return (0);
+}
