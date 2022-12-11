c5c7438aec69dda812edc20fe70048e3effcbd5c
ProgressAce <daiyaansallie7@gmail.com>
2022-12-11 00:57:21 -0800

    Update commenting for betty compliance

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
-int main(void) /* C Program that prints a certain text using the puts function */
+/* 
+ * main - C Program that prints a certain text using the puts function 
+ * */
+int main(void) 
 {
 	puts("\"Programming is like building a multilingual puzzle");
 
