2379cfc70a1c103bfa613dd8a38f114ce7ef8bfe
ProgressAce <daiyaansallie7@gmail.com>
2022-12-11 01:51:00 -0800

    Fix syntax errors

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,10 +17,11 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* Check whether n is positive, negative or zero */
 	if (n > 0)
-		puts("%d is positive", n)
+		printf("%d is positive", n);
 	if (n == 0)
-		puts("%d is zero", n)
+		printf("%d is zero", n);
 	if (n < 0)
-		puts("%d is negative", n)
+		printf("%d is negative", n);
+
 	return (0);
 }
