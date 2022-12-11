5e2a5165a69fe65dd53db6331d9c8930fbad2c18
ProgressAce <daiyaansallie7@gmail.com>
2022-12-11 08:03:35 -0800

    Fix bugs

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,6 @@
  */
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 60);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
 	return (0);
 }
