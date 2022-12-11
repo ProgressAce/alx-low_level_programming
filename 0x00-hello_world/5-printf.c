185de44acaa0064c5aab10f14eddd80db4263b1f
ProgressAce <daiyaansallie7@gmail.com>
2022-12-11 01:12:55 -0800

    Update commenting for betty compliance

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-/** 
+/**
  * main - C Program that prints a certain text using the printf function
  *
- * return: 0
+ * Return: 0
  */
-int main(void) 
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 
