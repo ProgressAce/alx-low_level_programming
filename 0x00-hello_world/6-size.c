eb24e26f9c0de59e515e03519e54c77233cace9c
ProgressAce <daiyaansallie7@gmail.com>
2022-12-11 00:00:09 -0800

    Update with correct output

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,12 @@
 #include <stdio.h>
 
+/* C Program that prints the size of various data types according to the computer it runs on */
 int main(void)
 {
-	char;
-	int;
-	long int;
-	long long int;
-	float;
 	printf("Size of a char: %d byte(s)",sizeof(char));
-	printf("Size of a char: %d byte(s)",sizeof(char));
-	printf("Size of a char: %d byte(s)",sizeof(char));
-	printf("Size of a char: %d byte(s)",sizeof(char));
-	printf("Size of a char: %d byte(s)",sizeof(char));
+	printf("Size of a int: %d byte(s)",sizeof(int));
+	printf("Size of a long int: %d byte(s)",sizeof(long int));
+	printf("Size of a long long int: %d byte(s)",sizeof(long long int));
+	printf("Size of a float: %d byte(s)",sizeof(float));
 	return (0);
 }
