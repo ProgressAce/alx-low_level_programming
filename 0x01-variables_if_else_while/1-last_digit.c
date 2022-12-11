b6a40179911d23ce3c1cc9d8375923073611e735
ProgressAce <daiyaansallie7@gmail.com>
2022-12-11 08:52:37 -0800

    Declare variable at the top and correct variable spelling

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,18 +13,19 @@
 int main(void)
 {
 	int n;
+	int lastDigit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
 	/* check whether last digit is greater than, less or equal to zero */
-	int lastDigit = n % 10;
+	lastDigit = n % 10;
 	if (lastDigit > 5)
-		printf("Last digit of %d is %d and is greater than 5\n", n, LastDigit);
+		printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
 	if (lastDigit > 5)
-		printf("Last digit of %d is %d and is greater than 5\n", n, LastDigit);
+		printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
 	if (lastDigit > 5)
-		printf("Last digit of %d is %d and is greater than 5\n", n, LastDigit);
+		printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
 
 	return (0);
 }
