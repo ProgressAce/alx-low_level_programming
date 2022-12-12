248f6fd331cc1fd27988f58a9afed285d5e81bb6
ProgressAce <daiyaansallie7@gmail.com>
2022-12-12 12:04:46 -0800

    hotfix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,8 @@
 /**
  * main - entry point
  *
- * Description: C Program that prints the size of various data types according to the computer it runs on
+ * Description: C Program that prints the size of various
+ * data types according to the computer it runs on
  *
  * Return: Always 0 (Success)
  */
@@ -11,8 +12,8 @@ int main(void)
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
 	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %d byte(s)\n", 
-			sizeof(long long int));
+	printf("Size of a long long int: ");
+	printf("%d byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
 }
