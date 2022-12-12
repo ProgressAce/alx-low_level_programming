9cb52c2b0f5e84ebdcd14764af3284f477da81bb
ProgressAce <daiyaansallie7@gmail.com>
2022-12-12 01:59:04 -0800

    Add 5-print_numbers.c source code

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Program that prints all single digit numbers of base 10 starting from 0, followed by a new line.
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int number = 0;
+
+	while (number < 10)
+	{
+		putchar(number);
+
+		number++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
