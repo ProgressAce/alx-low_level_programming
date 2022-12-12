a9784fb5589b75da3116c8c6aaff4bd6a8fef0c0
ProgressAce <daiyaansallie7@gmail.com>
2022-12-12 01:54:01 -0800

    Add 4-print_alphabt.c program

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet in lowercase, except and e, followed by a new line. Using putchar function
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter == 'q' || letter == 'e')
+			continue;
+
+		putchar(letter);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
