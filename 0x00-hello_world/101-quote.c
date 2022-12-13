5302c6571d47398928c5a0b37f40bc058f067dd1
ProgressAce <daiyaansallie7@gmail.com>
2022-12-13 00:42:10 -0800

    Reduce line and whitespace fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,14 @@
 /**
  * main - entry point
  *
- * Description: C Program that prints certain text without using any functions from man (3) printf and man (3) puts
+ * Description: C Program that prints certain text without using
+ * any functions from man (3) printf and man (3) puts
  *
  * Return: 0 (Success)
  */
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 
-			2015-10-19\n", 59);
+	write(1, "and that piece of art is useful\" - Dora Korpar,", 48);
+	write(1, " 2015-10-19\n", 13);
 	return (1);
 }
