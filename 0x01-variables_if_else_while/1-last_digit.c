18fec01b41c893a4e12fde92d6de8c022ce365a3
ProgressAce <daiyaansallie7@gmail.com>
2022-12-13 01:04:41 -0800

    Reduce line problem for betty compliance

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,13 +21,19 @@ int main(void)
 	/* check whether last digit is greater than, less or equal to zero */
 	lastDigit = n % 10;
 	if (lastDigit > 5)
-		printf("Last digit of %d is %d and is greater than 5\n", 
-				n, lastDigit);
+	{
+		printf("Last digit of %d is %d", n, lastDigit);
+		printf(" and is greater than 5\n");
+	}
+
 	if (lastDigit == 0)
 		printf("Last digit of %d is %d and is 0\n", n, lastDigit);
+
 	if (lastDigit < 6 && lastDigit != 0)
-		printf("Last digit of %d is %d and is less than 6 and 
-				not 0\n", n, lastDigit);
+	{
+		printf("Last digit of %d is %d", n, lastDigit);
+		printf(" and is less than 6 and not 0\n");
+	}
 
 	return (0);
 }
