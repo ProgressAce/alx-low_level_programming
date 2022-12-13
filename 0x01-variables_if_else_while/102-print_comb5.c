b0c049de8453eca7d45971e8c6ea6bd6b2f30b4a
ProgressAce <daiyaansallie7@gmail.com>
2022-12-13 01:55:25 -0800

    Add more for loops for each digit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -15,22 +15,26 @@ int main(void)
 	int num3;
 	int num4;
 
-	for (num1 = '0'; num1 <= '98'; num1++)
+	for (num1 = '0'; num1 <= '9'; num1++)
 	{
-		for (num2 = num1 + 1; num2 <= '99'; num2++)
+		for (num2 = 0; num2 <= '8'; num2++)
 		{
-			if (num1 <= '9')
-				putchar('0');
+			for (num3 = 0; num3 <= '9'; num3++)
+			{
+				for (num4 = num2 + 1; num4 <= 9; num4++)
+				{
+					putchar(num1);
+					putchar(num2);
 
-			putchar(num1);
-			putchar(' ');
+					putchar(' ');
 
-			if (num2 <= '9')
-				putchar('0');
+					putchar(num3);
+					putchar(num4);
 
-			putchar(num2);
-			putchar(',');
-			putchar(' ');
+					putchar(',');
+					putchar(' ');
+				}
+			}
 		}
 	}
 
