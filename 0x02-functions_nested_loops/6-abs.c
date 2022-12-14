b7af6a1999412535b9569716b467760d31e4141e
ProgressAce <daiyaansallie7@gmail.com>
2022-12-14 06:26:56 -0800

    Create 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "main.h"
+
+/* function prototype for abs */
+int abs(int n);
+
+/**
+ * _abs - prints the absolute value of a number
+ * @n: the number whose absolute value is to be printed
+ *
+ * Return: always 0
+ */
+
+int _abs(int n)
+{
+	return (abs(n));
+}
