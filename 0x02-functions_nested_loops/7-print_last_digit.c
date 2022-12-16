ab2f03a71148408757d24108646e4f6eeb1d4610
ProgressAce <daiyaansallie7@gmail.com>
2022-12-16 05:18:50 -0800

    Give desired output

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,12 +2,19 @@
 
 /**
  * print_last_digit - prints the last digit of a number
+ * if number is negative then make it positive.
  * @n: the number
  *
- * Return: always 0
+ * Return: last digit of n
  */
 
 int print_last_digit(int n)
 {
+	if (n < 0)
+		n = -n;
+
+	/* print the last digit of n */
+	_putchar((n % 10) + '0');
+
 	return (n % 10);
 }
