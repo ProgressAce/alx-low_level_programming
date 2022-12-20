f2b16e9e0b4e4f7c2bff144dfdb9a71390692f94
ProgressAce <daiyaansallie7@gmail.com>
2022-12-20 12:27:04 -0800

    Create reset_to_98 function

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - changes the value stored in a pointer's address to "98"
+ * @n: integer pointer to be manipulated
+ *
+ * Return: nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
