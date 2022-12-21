e01db0f4822ce44402fbc814c0f6698077c18892
ProgressAce <daiyaansallie7@gmail.com>
2022-12-20 20:48:56 -0800

    Fix segmentation fault

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,19 +10,18 @@
 void print_rev(char *s)
 {
 	int i;
-	int length;
+	int length = 0;
 
 	/* get the length of string "s" */
-	for (i = 0; s[i] != '\n'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 		length++;
 
-	_putchar(s[length - 1]);
 	/* reverse the string starting at the end */
-	/*while (length >= 0)
+	while (length >= 0)
 	{
-		_putchar(s[length]);
-
 		length--;
-	}*/
+
+		_putchar(s[length]);
+	}
 	_putchar('\n');
 }
