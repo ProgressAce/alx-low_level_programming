ec9bb612520c6463ec86ed2244445369be7c4934
ProgressAce <daiyaansallie7@gmail.com>
2022-12-30 14:47:59 -0800

    Fix desired output for odd length strings

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -22,7 +22,7 @@ void puts_half(char *str)
 	if (length % 2 == 0)
 		n = length / 2;
 	else
-		n = (length - 1) / 2;
+		n = (length - 1) / 2 + 1;
 
 	/* print second half of string */
 	for (n = n; n < length; n++)
