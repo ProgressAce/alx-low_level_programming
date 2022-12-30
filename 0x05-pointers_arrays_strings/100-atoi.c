c454c100d908d4072db8ce5f93a80aacf644636e
ProgressAce <daiyaansallie7@gmail.com>
2022-12-30 14:31:04 -0800

    Create _atoi function file

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * _atoi - converts the first number in a string to an integer.
+ * @s: pointer to the string
+ *
+ * Return: the converted integer
+ */
+
+int _atoi(char *s)
+{
+	int i;
+	int found = 0;
+	int sign = 1;
+	unsigned int total = 0;
+
+	/* loop through s */
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		/* if element is a digit, then loop */
+		while ((s[i] >= '0') && (s[i] <= '9'))
+		{
+			/* check if number is negative */
+			if (s[i - 1] == '-')
+				sign *= -1;
+
+			/* place next digit and convert it to an integer */
+			total = (total * 10) + (s[i] - '0');
+
+			found = 1;
+			i++;
+		}
+		/* check if a number was alrady found */
+		if (found == 1)
+			break;
+	}
+
+	total *= sign;
+
+	return (total);
+}
