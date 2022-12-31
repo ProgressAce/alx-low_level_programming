e96e3741df84971cf2eb4dec0dede8fa19e29d31
ProgressAce <daiyaansallie7@gmail.com>
2022-12-31 06:26:13 -0800

    Create cap_string function file

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalises all the words in a string.
+ * @s: pointer to the string
+ *
+ * Return: pointer s
+ */
+
+char *cap_string(char *s)
+{
+	int i;
+	int j;
+	int length = 0;
+	char separators[13] = {' ', '\t', '\n', ',', ';', '.',
+				'!', '?', '"', '(', ')', '{', '}'};
+
+	/* get length of string */
+	for (i = 0; s[i] != '\0'; i++)
+		length++;
+
+	/* check for, if length <= 0 */
+	if (length <= 0)
+		return ("");
+
+	/* loop through s */
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 13; j++)
+		{
+			if (s[i] == separators[j])
+			{
+				if ((s[i + 1] >= 97) && (s[i + 1] <= 122))
+					s[i + 1] = s[i + 1] - 32;
+			}
+		}
+	}
+
+	return (s);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,7 +1,8 @@
 int _putchar(char);
-char *_strcat(char*, char*);
-char *_strncat(char*, char*, int);
-int _strcmp(char*, char*);
-char *_strncpy(char*, char*, int);
+char *_strcat(char *, char *);
+char *_strncat(char *, char *, int);
+int _strcmp(char *, char *);
+char *_strncpy(char *, char *, int);
 void reverse_array(int *a, int n);
-char *string_toupper(char*);
+char *string_toupper(char *);
+char *cap_string(char *);
