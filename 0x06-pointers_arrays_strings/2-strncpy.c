4fc72edba3effc85295d3fc415192a9c95608c5a
ProgressAce <daiyaansallie7@gmail.com>
2022-12-30 23:03:45 -0800

    Fix desired output for lengths < 0

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -3,7 +3,7 @@
 /**
  * _strncpy - copies the first "n" characters of a string and
  * overwrites it into another string.
- * @dest: pointer to the array destination for the copied string
+ * @dest: pointer to destination for the copied string
  * @src: pointer to the string to be copied
  * @n: the number of characters
  *
@@ -13,30 +13,34 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
-	int len_dest = 0;
 	int len_src = 0;
 
-	/* get length of dest and src (address value's)*/
-	for (i = 0; dest[i] != '\0'; i++)
-		len_dest++;
-
+	/* get length of strings at src (address value's) */
 	for (i = 0; src[i] != '\0'; i++)
 		len_src++;
 
-	/*
-	 * loop through dest and overwrite passing index with
-	 * src until src nth element
-	 */
-	for (i = 0; i < n; i++)
+	/* check if n is a positive number, else put null byte into dest */
+	if (n > 0)
 	{
-		if (i > len_src)
-			dest[i] = '0';
-		else
-			dest[i] = src[i];
-	}
+		/*
+		 * loop through dest and overwrite passing index with
+		 * src until src nth element
+		 */
+		for (i = 0; i < n; i++)
+		{
+			if (i >= len_src)
+				/* add "0" for characters after len_src */
+				dest[i] = '0';
+			else
+				dest[i] = src[i];
+		}
 
-	if (len_src < n)
-		dest[i] = '\0';
+		/* append null character only if len_src(source string) <= n */
+		if (n > len_src)
+	      		dest[len_src] = '\0';
+	}
+	else
+		dest[0] = '\0';
 
 	return (dest);
 }
