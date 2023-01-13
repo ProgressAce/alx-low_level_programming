6c752dfc6d46957598c08b2af9585b0d720a9aee
ProgressAce <daiyaansallie7@gmail.com>
2023-01-13 09:43:05 -0800

    Create malloc_check function

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory using malloc. If malloc fails,
+ * it should execute exit status of value 98.
+ * @b: the amount of memory to allocate
+ *
+ * Return: pointer to the allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	char *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98);
+
+	return(ptr);
+}
