96e3bfe5e6e2f78100a4b86af0c986e42a5bfa16
ProgressAce <daiyaansallie7@gmail.com>
2023-01-13 15:53:13 -0800

    Create _realloc function

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _realloc - reallocates a block of memory, using malloc and free.
+ * @ptr: pointer to the old memory block
+ * @old_size: size of the old memory block
+ * @new_size: amount of bytes to be allocated to the new memory block
+ *
+ * Return: pointer to the new block of memory.
+ * if malloc fails, return NULL.
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *space;
+	int i;
+
+	ptr = (char *)ptr;
+	if (new_size == old_size)
+		return (ptr);
+
+	/* allocate memory block */
+	space = malloc(new_size);
+	if (space == NULL)
+		return (NULL);
+
+	/* check for empty arguments */
+	if ((new_size == 0) && (ptr != NULL))
+	{
+		free(ptr);
+		return (NULL);
+	}
+	else if (ptr != NULL)
+	{
+		/* initialise new_space memory block */
+		for (i = 0; (((char *) ptr)[i] != '\0') && (i < new_size); i++)
+			space[i] = ((char *) ptr)[i];
+	}
+
+	free(ptr);
+	return (space);
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,3 +2,4 @@ int _putchar(char c);
 void *malloc_checked(unsigned int b);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
