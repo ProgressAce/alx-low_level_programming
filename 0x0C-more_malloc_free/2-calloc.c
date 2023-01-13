5ec10f84bdf6a8c15ed12b5f097bb319650fc266
ProgressAce <daiyaansallie7@gmail.com>
2023-01-13 15:08:03 -0800

    Create _calloc function

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - allocates memory for an array of 'nmemb' elements
+ * of 'size' bytes each, using malloc.
+ * @nmemb: the number of elements of array
+ * @size: the amount of bytes for each element
+ * 
+ * Extra info: the memory is set to zero.
+ * if nmemb or size equals to zero, then the function returns NULL.
+ *
+ * Return: pointer to the allocated memory,
+ * if malloc fails, return NULL.
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *a;
+
+	/* check for arguments with value of 0 */
+	if ((nmemb == 0) || (size == 0))
+		return (NULL);
+
+	/* allocate memory and check if malloc returned successfully */
+	a = malloc(nmemb * size);
+	if (a == NULL)
+		return (NULL);
+
+	*a = 0;
+	return (a);
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,2 +1,3 @@
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
+void *_calloc(unsigned int nmemb, unsigned int size);
