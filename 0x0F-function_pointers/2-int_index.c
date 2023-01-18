d0bf4ebca31de1f39ebb19f0ef86ce24c61acf61
ProgressAce <daiyaansallie7@gmail.com>
2023-01-18 11:28:55 -0800

    Create int_index function

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * int_index - searches for an integer in an array that matches
+ * what another function is searching for.
+ * @array: the array of integers
+ * @size: size of array
+ * @cmp: pointer to function
+ *
+ * Return: the index of the first matched element
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+	        
+	if (size <= 0)
+		return (-1);
+
+	i = 0;
+	while (i < size)
+	{
+		/**
+		 * compare each element in an array according to
+		 * the function cmp points to
+		 */
+		if (cmp(array[i]))
+			return (i);
+
+		i++;
+	}
+
+	/* no element was matched */
+	return (-1);
+}
