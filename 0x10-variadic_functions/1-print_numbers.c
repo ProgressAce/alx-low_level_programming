cc5a402e596a4fc7b55aa183c3a1f3ea327820cd
ProgressAce <daiyaansallie7@gmail.com>
2023-01-19 07:49:07 -0800

    Variadic number function

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,35 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints all the numbers in its parameters,
+ * each separated by a string
+ * @separator: the string separator
+ * @n: the number of parameters of the function
+ * @...: the number arguments to be passed
+ *
+ * Return: nothing
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	int i;
+	/* declare pointer to agrument list */
+	va_list ptr;
+
+	/* initialise argument list */
+	va_start(ptr, n);
+
+	for (i = 0; i < n; i++)
+	{
+		/**
+		 * if it is the last argument of list,
+		 * don't add separator
+		 */
+		if (separator == NULL || n - 1 == i)
+			printf("%d", va_arg(ptr, int));
+		else
+			printf("%d%s", va_arg(ptr, int), separator);
+	}
+
+	printf("\n");
+}
