6d697f654276dab86e4aecc53cea0ba6643ef461
ProgressAce <daiyaansallie7@gmail.com>
2023-01-21 04:01:50 -0800

    calc program header file

---
+++ 0x0F-function_pointers/3-calc.h
@@ -0,0 +1,25 @@
+#ifndef CALC
+#define CALC
+
+#include <stdio.h>
+
+int op_add(int, int);
+int op_sub(int, int);
+int op_mul(int, int);
+int op_div(int, int);
+int op_mod(int, int);
+int (*get_op_func(char *s)) (int, int);
+
+/**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+typedef struct op
+{
+	char *op;
+	int (*f)(int a, int b);
+} op_t;
+
+#endif /* #ifndef _CALC_ */
