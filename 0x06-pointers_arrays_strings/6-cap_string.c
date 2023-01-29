0982d5ba4e5856e583d4938d1a8c6208661f6dd1
ProgressAce <daiyaansallie7@gmail.com>
2023-01-28 22:05:58 -0800

    Check for first char of str first

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -15,22 +15,14 @@ char *cap_string(char *s)
 	char separators[13] = {' ', '\t', '\n', ',', ';', '.',
 				'!', '?', '"', '(', ')', '{', '}'};
 
-	/* get length of string */
-	for (i = 0; s[i] != '\0'; i++)
-		length++;
-
-	/* check for, if length <= 0 */
-	if (length == 0)
-		return (s);
-
 	/* loop through s */
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		/* loop through leet characters */
 		for (j = 0; j < 13; j++)
 		{
-			/* check if s char == separators char */
-			if (s[i] == separators[j])
+			/* check if s char == separators char and first char */
+			if (i == 0 || s[i] == separators[j])
 			{
 				if ((s[i + 1] >= 97) && (s[i + 1] <= 122))
 					s[i + 1] = s[i + 1] - 32;
