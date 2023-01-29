25324ce48ad0d064aada2701712f39efe92106b6
ProgressAce <daiyaansallie7@gmail.com>
2023-01-28 22:12:48 -0800

    Fix str indexing issue

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,20 +9,25 @@
 
 char *cap_string(char *s)
 {
-	int i;
+	int i = 0;
 	int j;
 	int length = 0;
 	char separators[13] = {' ', '\t', '\n', ',', ';', '.',
 				'!', '?', '"', '(', ')', '{', '}'};
 
+	/* check first letter  of the string separately */
+	if ((s[i] >= 97) && (s[i] <= 122))
+		s[i] = s[i] - 32;
+
+
 	/* loop through s */
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 1; s[i] != '\0'; i++)
 	{
 		/* loop through leet characters */
 		for (j = 0; j < 13; j++)
 		{
-			/* check if s char == separators char and first char */
-			if (i == 0 || s[i] == separators[j])
+			/* check if s char == separators char */
+			if (s[i] == separators[j])
 			{
 				if ((s[i + 1] >= 97) && (s[i + 1] <= 122))
 					s[i + 1] = s[i + 1] - 32;
