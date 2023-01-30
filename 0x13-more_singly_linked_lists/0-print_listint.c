d2ec7a0f510e647cc86c40ff8987298c32895926
ProgressAce <daiyaansallie7@gmail.com>
2023-01-30 08:49:26 -0800

    Create function and add itsprototype

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a lnked listint_t list.
+ * @h: the linked list
+ *
+ * Return: the number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}
