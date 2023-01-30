40b902a28da3291282c07c4ae064d7e5c870145a
ProgressAce <daiyaansallie7@gmail.com>
2023-01-30 01:04:52 -0800

    Create header file with listint_t struct and print_listint prototype

---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,24 @@
+#ifndef LISTS
+#define LISTS
+
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * 
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+
+/* prototypes */
+size_t print_listint(const listint_t *h);
+
+
+#endif /* LISTS */
