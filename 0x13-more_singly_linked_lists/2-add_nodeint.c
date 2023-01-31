f49f2d011ac939ff89f66bb60d61857720eb5810
ProgressAce <daiyaansallie7@gmail.com>
2023-01-31 05:04:28 -0800

    Add malloc NULL check

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -10,15 +10,18 @@
 
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *new;
+	listint_t *new_node;
 
-	new = malloc(sizeof(listint_t));
+	new_node = malloc(sizeof(listint_t));
 
-	new->n = n;
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
 
 	/* connect nodes */
-	new->next = *head;
-	*head = new;
+	new_node->next = *head;
+	*head = new_node;
 
-	return (new);
+	return (new_node);
 }
