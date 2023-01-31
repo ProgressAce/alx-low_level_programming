af05e29d01f29aeef2074ae611e24e5b10b531d0
ProgressAce <daiyaansallie7@gmail.com>
2023-01-30 19:59:32 -0800

    Create add_nodeint_end function

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -8,6 +8,35 @@
  * Return: the address of the new_node
  */
 
-listint_t *add_nodeint_end(listint_t *h, const int n)
+listint_t *add_nodeint_end(listint_t **head, const int n)
 {
+	listint_t *new, *curr = *head;
+	
+	new = malloc(sizeof(listint_t));
+
+	if (new == NULL)
+		return (NULL);
+	else
+	{
+		/* assign values to new node */
+		new->n = n;
+		new->next = NULL;
+	}
+
+	if (*head != NULL)
+	{
+		curr = *head;
+		while (curr->next != NULL)
+			curr = curr->next;
+
+		/* connect nodes */
+		curr->next = new;
+
+		return (new);
+	}
+	else
+		/* connect nodes */
+		*head = new;
+
+	return (NULL);
 }
