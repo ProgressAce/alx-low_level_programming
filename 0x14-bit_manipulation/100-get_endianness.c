53aca38d97353a0a3f532cbd728797f7ee9fa9ff
ProgressAce <daiyaansallie7@gmail.com>
2023-02-03 12:19:34 -0800

    Create get_endianess function with prototype

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks the endianness of the system
+ * @void
+ *
+ * Return: 0 for big endianess, 1 for little endianess
+ */
+
+int get_endianness(void)
+{
+	unsigned int x = 1;
+	char *c = (char *) &x;
+
+	return (int) *c;
+}
