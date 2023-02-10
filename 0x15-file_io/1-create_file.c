1bf6fff12d3c43c359187d2385814bcb6111bdcf
ProgressAce <daiyaansallie7@gmail.com>
2023-02-10 11:59:43 -0800

    Hotfix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	/* conditions for creation of file */
-	fd = open(filename, O_CREAT | O_EXCL | O_TRUNC | O_WRONLY, 0600);
+	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 	if (fd < 0)
 		return (-1);
 
