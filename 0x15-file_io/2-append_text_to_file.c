bcc6e9cfb518b86a041d7acd4ca8487d2f6bc489
ProgressAce <daiyaansallie7@gmail.com>
2023-02-10 11:48:40 -0800

    Create function for appending to a file

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,62 @@
+#include "main.h"
+
+int _strlen(char *s);
+
+/**
+ * append_text_to_file - appends text at the end of a file.
+ * @filename: name of the file
+ * @text_content: null-terminated string to write to the file
+ *
+ * Extra: file is not created if filename already exists.
+ * if text_content is NULL, do not add anything to the file.
+ *
+ * Return: 1 on success, or -1 on failure.
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, wbytes, text_len;
+
+	if (filename == NULL)
+		return (-1);
+
+	/* opening file for appending */
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd < 0)
+		return (-1);
+
+	/* case for empty file */
+	if (text_content == NULL)
+		return (-1);
+
+	/* write to file */
+	text_len = _strlen(text_content);
+	wbytes = write(fd, text_content, text_len);
+
+	if (wbytes < text_len)
+		return (-1);
+
+	close(fd);
+	return (1);
+}
+
+/**
+ * _strlen - returns the length of a string passed to it.
+ * @s: the string which will have its length returned.
+ *
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int i;
+	int length;
+
+	length = 0;
+
+	/* loop until end of string s */
+	for (i = 0; s[i] != '\0'; i++)
+		length++;
+
+	return (length);
+}
