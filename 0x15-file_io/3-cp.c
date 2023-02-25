fc8e7945ee4331ee188aaeef74e4bc25071c797e
ProgressAce <daiyaansallie7@gmail.com>
2023-02-24 19:59:56 -0800

    hoytfix

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,70 +1,138 @@
+#include <stdio.h>
 #include "main.h"
 
-void error_file(int fd_copy, int fd_paste, char **argv);
-
 /**
- * main - program that copies the content of a file to another file.
- * @argc: number of arguments
- * @argc: an array of string arguments
- *
- * Return: the value of the bit at index "index", or -1 if an error occured
- */
-
+  * main - Entry point
+  * @argc: The argument count
+  * @argv: The argument vector
+  *
+  * Return: ...
+  */
 int main(int argc, char **argv)
 {
-	int fd_copy, fd_paste;
-	ssize_t nchars = 1024, wbytes;
-	char buff[1024];
-
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 
-	while (nchars == 1024)
-	{
-		fd_copy = open(argv[1], O_RDONLY);
-		fd_paste = open(argv[2], O_CREAT | O_WRONLY | O_APPEND | O_TRUNC, 0664);
-		error_file(fd_copy, fd_paste, argv);
-
-		/* read from copy file and check for errors */
-		nchars = read(fd_copy, buff, nchars);
-		if (nchars < 0)
-			error_file(-1, 0, argv);
-
-		/* write to paste file and check for errors */
-		wbytes = write(fd_paste, buff, nchars);
-		if (wbytes < 0)
-			error_file(0, -1, argv);
-
-		/* check for any close errors on system call */
-		if (close(fd_copy) == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_copy);
-			exit(100);
-		}
-		if(close(fd_paste) == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_paste);
-			exit(100);
-		}
-	}
-
-	return (1);
+	copy_file(argv[1], argv[2]);
+	exit(0);
 }
 
-void error_file(int fd_copy, int fd_paste, char **argv)
+/**
+  * copy_file - ...
+  * @src: ...
+  * @dest: ...
+  *
+  * Return: ...
+  */
+void copy_file(const char *src, const char *dest)
 {
-	if (fd_copy == -1)
+	int ofd, tfd, readed;
+	char buff[1024];
+
+	ofd = open(src, O_RDONLY);
+	if (!src || ofd == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", src);
 		exit(98);
 	}
 
-	if (fd_paste == -1)
+	tfd = open(dest, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	while ((readed = read(ofd, buff, 1024)) > 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
+		if (write(tfd, buff, readed) != readed || tfd == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", dest);
+			exit(99);
+		}
+	}
+
+	if (readed == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", src);
+		exit(98);
+	}
+
+	if (close(ofd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", ofd);
+		exit(100);
+	}
+
+	if (close(tfd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", tfd);
+		exit(100);
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
