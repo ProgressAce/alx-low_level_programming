ee80661f5915722026d87cc8d05d7ee8adb7bb83
ProgressAce <daiyaansallie7@gmail.com>
2023-02-26 02:00:05 -0800

    test

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,15 +1,16 @@
-#ifndef FILE_IO
-#define FILE_IO
+#ifndef MAIN_H
+#define MAIN_H
 
-#include <unistd.h>
-#include <stdio.h>
+#include <elf.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
 #include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
-/* function prototpyes */
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
-
-#endif /* FILE_IO */
+#endif
