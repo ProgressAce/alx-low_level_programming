241e4d4cc9b0609b87f0222bcaaf8838e9d8b8ef
SubtleDaiyaan <daiyaansallie7@gmail.com>
2023-06-09 15:18:20 -0700

    check argument correctness

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,22 +12,24 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *hash_table;
 	unsigned long int i;
 
-	/*  */
+	if (size == 0)
+		return (NULL);
+
+	/* memory for hash table structure */
 	hash_table = malloc(sizeof(hash_table_t));
 	if (hash_table == NULL)
 		return (NULL);
 
 	hash_table->size = size;
 
-	/*  */
-	hash_table->array = malloc( sizeof(hash_node_t) * size);
+	/* memory for nodes of the hash table's array */
+	hash_table->array = malloc(sizeof(hash_node_t) * size);
 	if (hash_table->array == NULL)
 	{
 		free(hash_table);
 		return (NULL);
 	}
 
-	/*  */
 	for (i = 0; i < hash_table->size; i++)
 		hash_table->array[i] = NULL;
 
