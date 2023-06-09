f0a69c1e86abde5590f59d5bc1152158f0dfd4fa
SubtleDaiyaan <daiyaansallie7@gmail.com>
2023-06-09 13:05:43 -0700

    fix final bugs with linked list

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,15 +10,22 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_table;
+	unsigned long int i;
 
 	hash_table = malloc(sizeof(hash_table_t));
 	if (hash_table == NULL)
 		return (NULL);
 
+	if (size <= 0)
+		return (NULL);
 	hash_table->size = size;
+
 	hash_table->array = malloc(size * sizeof(hash_node_t));
 	if (hash_table->array == NULL)
 		return (NULL);
+
+	for (i = 0; i < hash_table->size; i++)
+		hash_table->array[i] = NULL;
 
 	return (hash_table);
 }
--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -27,16 +27,17 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	index = key_index((unsigned char *) key, ht->size);
 
-	if (ht->array[index] == NULL)
+	head = ht->array[index];
+	if (head == NULL)
 	{
 		node = create_node(key, value);
 		strcpy(node->key, key);
 		strcpy(node->value, value);
 		node->next = NULL;
+		ht->array[index] = node;
 	}
 	else
 	{
-		head = ht->array[index];
 		while (head)
 		{
 			if (strcmp(head->key, key) == 0)
