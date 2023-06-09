1da4bd5bebdff21ef116779bc366bd3bc13a184c
SubtleDaiyaan <daiyaansallie7@gmail.com>
2023-06-09 07:21:07 -0700

    spaces to tab for betty

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,16 +9,16 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-        hash_table_t *hash_table;
+	hash_table_t *hash_table;
 
-        hash_table = malloc(sizeof(hash_table_t));
-        if (hash_table == NULL)
-                return (NULL);
+	hash_table = malloc(sizeof(hash_table_t));
+	if (hash_table == NULL)
+		return (NULL);
 
-        hash_table->size = size;
-        hash_table->array = malloc(size * sizeof(hash_node_t));
-        if (hash_table->array == NULL)
-                return (NULL);
+	hash_table->size = size;
+	hash_table->array = malloc(size * sizeof(hash_node_t));
+	if (hash_table->array == NULL)
+		return (NULL);
 
-        return (hash_table);
+	return (hash_table);
 }
