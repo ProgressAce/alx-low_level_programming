9781dac481f996e3f8b13ed9f3443dcd875b396f
SubtleDaiyaan <daiyaansallie7@gmail.com>
2023-06-09 14:44:03 -0700

    free hash table on failure, fix the node that index in the array points to

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,18 +12,22 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *hash_table;
 	unsigned long int i;
 
+	/*  */
 	hash_table = malloc(sizeof(hash_table_t));
 	if (hash_table == NULL)
 		return (NULL);
 
-	if (size <= 0)
-		return (NULL);
 	hash_table->size = size;
 
-	hash_table->array = malloc(size * sizeof(hash_node_t));
+	/*  */
+	hash_table->array = malloc( sizeof(hash_node_t) * size);
 	if (hash_table->array == NULL)
+	{
+		free(hash_table);
 		return (NULL);
+	}
 
+	/*  */
 	for (i = 0; i < hash_table->size; i++)
 		hash_table->array[i] = NULL;
 
--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -54,6 +54,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		strcpy(node->key, key);
 		strcpy(node->value, value);
 		node->next = head;
+		ht->array[index] = node;
 	}
 
 	return (1);
