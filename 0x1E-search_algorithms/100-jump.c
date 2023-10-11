82f4e99f72044aef24cd7d53c918ca37a8f078a3
ProgressAce <daiyaansallie7@gmail.com>
2023-10-11 11:14:15 -0700

    add: task 100 101 solution

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,58 @@
+#include "search_algos.h"
+#include <math.h>
+
+/**
+ * jump_search - finds the first occurence of a value in an array,
+ * using the jump search algorithm.
+ * @array: the array
+ * @size: the size of the array
+ * @value: the value to search for in the array
+ *
+ * The jump size used is square root of the size of the array.
+ * Condition for accurate results:
+ * - The array should be sorted in ascending order.
+ *
+ * Return: the index of the value in the array, otherwise
+ * -1 if the array points to NULL or the value is not found.
+ */
+int jump_search(int *array, size_t size, int value)
+{
+	size_t i = 0, j, low = 0, high = 0, step = sqrt(size);
+
+	while (i < size) /* iterating array via specified jump steps */
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (value <= array[i])
+		{
+			printf("Value found between indexes [%ld] and [%ld]\n",
+				low, high);
+			for (j = low; j <= high; j++) /* linear search subpart */
+			{
+				printf("Value checked array[%ld] = [%d]\n",
+					j, array[j]);
+				if (array[j] == value)
+				{
+					return (j);
+				}
+			}
+			return (-1); /* value not found sorted subarray */
+		}
+		else
+		{
+			low = i;
+		}
+
+		/* ensure that index does not jump out of range */
+		if (i + step >= size && high != size - 1)
+		{
+			high = size - 1;
+			i += 1;
+		}
+		else
+		{
+			i += step;
+			high = i;
+		}
+	}
+	return (-1);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -7,5 +7,6 @@
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 void print_search(int *array, size_t left_i, size_t right_i);
+int jump_search(int *array, size_t size, int value);
 
 #endif /* SEARCH_ALGOS */
